f43f1e4d1b0a5ddb5303764f0293ca71f99968a7
philintheattic <154841043+philintheattic@users.noreply.github.com>
2024-01-08 14:17:38 +0100

    Added way to change font size

--- video2ascii.py
+++ video2ascii.py
@@ -1,7 +1,8 @@
-#TODO ##### STEP 1 ##### user inputs a video file
-#TODO ##### STEP 2 ##### ffmpeg extracts individual frames to a temp folder
-#TODO ##### STEP 3 ##### python script iterates over these images and turns them into asci art
-#TODO ##### STEP 4 ##### ffmpeg concatenates all the images back to a video
+##### STEP 1 ##### user inputs a video file
+##### STEP 2 ##### ffmpeg extracts individual frames to a temp folder
+##### STEP 3 ##### python script iterates over these images and turns them into asci art
+##### STEP 4 ##### ffmpeg concatenates all the images back to a video
+#TODO check for framerate and update the ffmpeg command accordingly. For now it only works with 25fps.
 
 from PIL import Image, ImageDraw, ImageFont
 import subprocess
@@ -11,12 +12,12 @@ import math
 
 # Header
 print("")
-print("Video 2 ASCII. Philipp Wachowitz 2023")
+print("Video 2 ASCII. Philipp Wachowitz 2024")
 print("Takes a video and turns it into ASCII Art")
 print("-----------------------------------------")
 print("")
 
-# Funktion für Fortschrittsbalken
+# Function for the progressbar
 def progress_bar(progress, total):
     percent = 100 * (progress/ float(total))
     bar = "█" * int(percent) + "-" * (100 - int(percent))
@@ -25,9 +26,17 @@ def progress_bar(progress, total):
 
 ##### STEP 1 - User inputs a video file #####
 
-
+# Get user input
 input_file = input("Input File: ")
 output_file = input("Output File: ")
+LOD_input = input("Level of Detail (lower value = more details, default is 1): ")
+
+# If input is left empty, default to 1
+if LOD_input == "":
+     LOD_input = 1
+
+# Make sure LOD is an integer
+LOD = math.floor(int(LOD_input))
 
 # Creating temp directories
 input_directory = "in"
@@ -53,40 +62,44 @@ subprocess.run(makeFrames())
 ##### STEP 3 - The main act #####
 
 
-# Read the ASCII Characters from external file. This makes changing it easy for the enduser
+# Read the ASCII Characters from external file. This makes changing it easy for the user
 with open("assets/chars.txt") as f:
     chars = f.read()
     f.close()
 
+# Turn the ASCII string into an array of characters
 charArray = list(chars)
 # Uncomment to reverse "colors"
 # charArray.reverse()
 charLength = len(charArray)
 mapping = charLength/256
-scaleFactor = 0.1        # can be changed depending on the input size. take care of later
+scaleFactor = 0.1        # Can be changed depending on the input size. Mess around at your own risk.
 
+# Function to turn a brightness value into a corresponding character from the array
 def getChar(input_pixel):
     return charArray[math.floor(input_pixel*mapping)]
 
 print("Turning frames into ASCII Art...")
 
 # Get infos for the progressbar
-file_count = 0
-file_count_2 = 0
+file_count_total = 0
+file_count_value = 0
 for files in os.listdir(input_directory):
-     file_count += 1
-progress_bar(0,file_count)
-
+     file_count_total += 1
+progress_bar(0,file_count_total)
 
 # Loop through all files in the input folder, turn them into ASCII Art and save them in the output folder
 for files in os.listdir(input_directory):
+    # Open image file
     image_file = f"in/{files}"
     image = Image.open(image_file)
 
-    oneCharWidth = 10
-    oneCharHeight = 10
+    # Set character width and height
+    oneCharWidth = int(10 * LOD)
+    oneCharHeight = int(10 * LOD)
 
-    font = ImageFont.truetype("assets/Cour.ttf", 10)
+    # Set font
+    font = ImageFont.truetype("assets/Cour.ttf", int(10 * LOD))
 
     # Resize Image to make processing faster
     width, height = image.size
@@ -95,25 +108,27 @@ for files in os.listdir(input_directory):
     pixels = image.load()
 
     # Create a new output image and prepare it for drawing
-    outputImage = Image.new("RGB", (int(oneCharWidth * width),int(oneCharHeight * height)), color = (0, 0, 0))
+    outputImage = Image.new("RGB", (int(oneCharWidth * width / LOD),int(oneCharHeight * height / LOD)), color = (0, 0, 0))
     d = ImageDraw.Draw(outputImage)
 
-    # check every pixel for color value, turn that into a brightness value and assign a char from the character array to it based on the brightness
-    for i in range(height):
-        for j in range(width):
+    # Check every pixel for color value, turn that into a brightness value and assign a char from the character array to it based on the brightness
+    for i in range(0, height, LOD):
+        for j in range(0, width, LOD):
             r, g, b = pixels[j, i]
             brightness = int((r + g + b)/3)
             pixels[j, i] = brightness
-            d.text((j * oneCharWidth, i * oneCharHeight), getChar(brightness), font = font, fill = (255, 255, 255))
+            d.text((j * oneCharWidth / LOD, i * oneCharHeight / LOD), getChar(brightness), font = font, fill = (255, 255, 255))
 
-    # save to file
+    # Save to file
     outputImage.save(f"out/{files}")
 
     # Move the progress bar forward
-    progress_bar(file_count_2+1,file_count)
-    file_count_2 += 1
+    progress_bar(file_count_value+1,file_count_total)
+    file_count_value += 1
+
 
 ##### STEP 4 - Reassemble the new frames into a video file #####
+
 
 print("")
 print("Making Movie...")
@@ -126,11 +141,11 @@ subprocess.run(makeMovie())
 
 print("Finished compiling movie!")
 
-# ask the user to remove all temp files
+# Ask the user to remove all temp files
 delete_prompt = input("Delete all temp files? [y/n] ")
 if (delete_prompt == "y"):
     shutil.rmtree(input_directory)
     shutil.rmtree(output_directory)
     input("Files deleted! You are all set! Press enter to exit and enjoy your ASCII video...")
 else:
-    print("Finished! Press enter to exit and enjoy your ASCII video...")    
+    input("Finished! Press enter to exit and enjoy your ASCII video...")    
